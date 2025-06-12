88518fc152fdbecf7aa822229dbc487acc90c5f3
J. Salvador Arias <jsalarias@gmail.com>
2025-06-12 19:13:20 +0300

    cmd/gbif: refactor filter taxonomy usage

--- cmd/gbifer/filter/filter.go
+++ cmd/gbifer/filter/filter.go
@@ -380,10 +380,10 @@ func filterCountries(r io.Reader, w io.Writer, tx *taxonomy.Taxonomy, tc map[int
 		if err != nil {
 			return fmt.Errorf("table %q: row %d: %v", input, ln, err)
 		}
-		if tx.Taxon(id).ID != id {
+		if tx.Taxon(id).ID == 0 {
 			continue
 		}
-		if tx.Rank(id) < taxonomy.Species {
+		if rk := tx.Rank(id); rk != taxonomy.Unranked && rk < taxonomy.Species {
 			continue
 		}
 
